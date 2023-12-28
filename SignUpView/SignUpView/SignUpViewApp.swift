b8fc694dd0d63478f51f7b356e3835ec3d8f109b
Alex Dmitriev <107206552+Carbofoss@users.noreply.github.com>
2023-12-29 00:21:22 +0300

    Add files via upload

---
+++ SignUpView/SignUpView/SignUpViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewApp.swift
+//  SignUpView
+//
+//  Created by MacBook on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SignUpViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
